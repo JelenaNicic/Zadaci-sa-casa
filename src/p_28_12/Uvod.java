bcad16b4eb09477bdc1ee3bb3f690df7b43eab4b
Jelena Nicic <nicicj9@gmail.com>
2021-12-28 19:10:10 +0100

    Print 100

--- src/p_28_12/Uvod.java
+++ src/p_28_12/Uvod.java
@@ -5,6 +5,13 @@ public class Uvod {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-	}
+		for(int i=0;i<10;i++) {
+			System.out.println(i);
+		}
+		for(int i=0;i<100;i++) {
+			System.out.println(i);
 
-}
+		
+		}}}
+
+	
